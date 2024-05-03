7e9c0f847fdf11a48786426762ddc35779588306
Victor_Sandi <victorsandi.okc@gmail.com>
2024-05-02 21:36:26 -0500

    start of gu

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Test")
+from tkinter import *
+import os
+
+root = Tk()
+img = PhotoImage(file=os.path.join(os.path.dirname(__file__), "messageicon.png"))
+root.iconphoto(True, img)
+root.wm_title("Secure P2P Messaging")
+
+
+
+root.mainloop()
